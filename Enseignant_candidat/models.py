c1fe95d7e96945a2908a6d80bbed5d74ea965e5a
Safa BENABDESSADOK <benabdessadok.safa@gmail.com>
2023-05-05 13:15:28 +0100

    Add field "id_emplacement" to "candidat"

--- Enseignant_candidat/models.py
+++ Enseignant_candidat/models.py
@@ -5,6 +5,47 @@ from django.utils.translation import gettext_lazy as _
 from Gestion.models import Utilisateur
 from Administrative_staff.models import Concours, Sujet
 
+class Enseignant(models.Model):
+    id_enseignant = models.OneToOneField(Utilisateur, on_delete=models.CASCADE, blank=False,
+                                        null=False, db_column='id_enseignant', to_field='id', primary_key=True)
+    grade = models.CharField(max_length=5, blank=False, null=False)
+    universite = models.CharField(max_length=200, null=False, blank=False)
+    faculte = models.CharField(max_length=100, blank=False, null=False)
+    depertement = models.CharField(max_length=100, blank=False, null=False)
+    specialite = models.CharField(max_length=100, blank=False, null=False)
+    id_sujet = models.ForeignKey(Sujet , on_delete=models.CASCADE, blank=False,
+                                    null=True, db_column='id_sujet', to_field='id_sujet')
+    nb_copie = models.PositiveIntegerField(default = 0, blank=False, null=False)
+
+    class Meta:
+        db_table = 'enseignant'
+
+class Emplacement(models.Model):
+    id_emplacement = models.AutoField(primary_key=True)
+    WILAYA = [
+        (1,"Adrar"), (2,"Chlef"), (3,"Laghouat"), (4,"Oum El Bouaghi"), (5,"Batna"), (6,"Béjaïa"), (7,"Biskra"), (8,"Bechar"), (9,"Blida"),
+        (10,"Bouira"), (11,"Tamanrasset"), (12,"Tbessa"), (13,"Tlemcen"), (14,"Tiaret"), (15,"Tizi Ouzou"), (16,"Alger"), (17,"Djelfa"),
+        (18,"Jijel"), (19,"Setif"), (20,"Saida"), (21,"Skikda"), (22,"Sidi Bel Abbes"), (23,"Annaba"), (24,"Guelma"), (25,"Constantine"),
+        (26,"Medea"), (27,"Mostaganem"), (28,"M'Sila"), (29,"Mascara"), (30,"Ouargla"), (31,"Oran"), (32,"El Bayadh"), (33,"Illizi"),
+        (34,"Bordj Bou Arreridj"), (35,"Boumerdes"), (36,"El Tarf"), (37,"Tindouf"), (38,"Tissemsilt"), (39,"El Oued"), (40,"Khenchela"),
+        (41,"Souk Ahras"), (42,"Tipaza"), (43,"Mila"), (44,"Ain Defla"), (45,"Naama"), (46,"Ain Temouchent"), (47,"Ghardaia"), (48,"Relizane"),
+        (49,"Timimoun"), (50,"Bordj Baji Mokhtar"), (51,"Ouled Djellal"), (52,"Béni Abbès"), (53,"In Salah"), (54,"In Guezzam"), (55,"Touggourt"),
+        (56,"Djanet"), (57,"El M'ghair"), (58,"El Menia"),
+    ]
+    wilaya = models.IntegerField(choices=WILAYA, null=False, blank=False)
+    daira = models.CharField(max_length=100, null=False, blank=False)
+    commune = models.CharField(max_length=100, null=False, blank=False)
+    universite = models.CharField(max_length=200, null=False, blank=False)
+    faculte = models.CharField(max_length=200, null=False, blank=False)
+    salle = models.CharField(max_length=100, null=False, blank=False)
+    capacite = models.PositiveIntegerField(default=0, null=False, blank=False)
+    id_enseignant_principal = models.ForeignKey(Enseignant, on_delete=models.CASCADE, blank=False,
+                                        null=True, db_column='id_enseignant_principal', to_field='id_enseignant', related_name='enseignant_principal')
+    id_enseignant_secondaire = models.ForeignKey(Enseignant, on_delete=models.CASCADE, blank=False,
+                                        null=True, db_column='id_enseignant_secondaire', to_field='id_enseignant', related_name='enseignant_secondaire')
+
+    class Meta:
+        db_table = 'emplacement'
 
 def validate_decimals(value):
     try:
@@ -26,25 +67,11 @@ class Candidat(models.Model):
     note_sujet1 = models.FloatField(default=0.00, validators=[validate_decimals], null=False, blank=False)
     note_sujet2 = models.FloatField(default=0.00, validators=[validate_decimals], null=False, blank=False)
     moyenne = models.FloatField(default=0.00, validators=[validate_decimals], null=False, blank=False)
+    id_emplacement = models.ForeignKey(Emplacement , on_delete=models.CASCADE, blank=False,
+                                    null=True, db_column='id_emplacement', to_field='id_emplacement')
 
     class Meta:
         db_table = 'candidat'
-
-
-class Enseignant(models.Model):
-    id_enseignant = models.OneToOneField(Utilisateur, on_delete=models.CASCADE, blank=False,
-                                        null=False, db_column='id_enseignant', to_field='id', primary_key=True)
-    grade = models.CharField(max_length=5, blank=False, null=False)
-    universite = models.CharField(max_length=200, null=False, blank=False)
-    faculte = models.CharField(max_length=100, blank=False, null=False)
-    depertement = models.CharField(max_length=100, blank=False, null=False)
-    specialite = models.CharField(max_length=100, blank=False, null=False)
-    id_sujet = models.ForeignKey(Sujet , on_delete=models.CASCADE, blank=False,
-                                    null=True, db_column='id_sujet', to_field='id_sujet')
-    nb_copie = models.PositiveIntegerField(default = 0, blank=False, null=False)
-
-    class Meta:
-        db_table = 'enseignant'
 
 class Notification_Candidats(models.Model):
     id_notification_candidat = models.AutoField(primary_key=True)
@@ -56,7 +83,6 @@ class Notification_Candidats(models.Model):
 
     class Meta:
         db_table = 'notification_candidats'
-
 
 class Notification_Enseignants(models.Model):
     id_notification_enseignant = models.AutoField(primary_key=True)
@@ -137,33 +163,6 @@ class Choix(models.Model):
     class Meta:
         unique_together = (('id_these', 'id_candidat'),)
         db_table = 'choix'
-
-class Emplacement(models.Model):
-    id_emplacement = models.AutoField(primary_key=True)
-    WILAYA = [
-        (1,"Adrar"), (2,"Chlef"), (3,"Laghouat"), (4,"Oum El Bouaghi"), (5,"Batna"), (6,"Béjaïa"), (7,"Biskra"), (8,"Bechar"), (9,"Blida"),
-        (10,"Bouira"), (11,"Tamanrasset"), (12,"Tbessa"), (13,"Tlemcen"), (14,"Tiaret"), (15,"Tizi Ouzou"), (16,"Alger"), (17,"Djelfa"),
-        (18,"Jijel"), (19,"Setif"), (20,"Saida"), (21,"Skikda"), (22,"Sidi Bel Abbes"), (23,"Annaba"), (24,"Guelma"), (25,"Constantine"),
-        (26,"Medea"), (27,"Mostaganem"), (28,"M'Sila"), (29,"Mascara"), (30,"Ouargla"), (31,"Oran"), (32,"El Bayadh"), (33,"Illizi"),
-        (34,"Bordj Bou Arreridj"), (35,"Boumerdes"), (36,"El Tarf"), (37,"Tindouf"), (38,"Tissemsilt"), (39,"El Oued"), (40,"Khenchela"),
-        (41,"Souk Ahras"), (42,"Tipaza"), (43,"Mila"), (44,"Ain Defla"), (45,"Naama"), (46,"Ain Temouchent"), (47,"Ghardaia"), (48,"Relizane"),
-        (49,"Timimoun"), (50,"Bordj Baji Mokhtar"), (51,"Ouled Djellal"), (52,"Béni Abbès"), (53,"In Salah"), (54,"In Guezzam"), (55,"Touggourt"),
-        (56,"Djanet"), (57,"El M'ghair"), (58,"El Menia"),
-    ]
-    wilaya = models.IntegerField(choices=WILAYA, null=False, blank=False)
-    daira = models.CharField(max_length=100, null=False, blank=False)
-    commune = models.CharField(max_length=100, null=False, blank=False)
-    universite = models.CharField(max_length=200, null=False, blank=False)
-    faculte = models.CharField(max_length=200, null=False, blank=False)
-    salle = models.CharField(max_length=100, null=False, blank=False)
-    capacite = models.PositiveIntegerField(default=0, null=False, blank=False)
-    id_enseignant_principal = models.ForeignKey(Enseignant, on_delete=models.CASCADE, blank=False,
-                                        null=True, db_column='id_enseignant_principal', to_field='id_enseignant', related_name='enseignant_principal')
-    id_enseignant_secondaire = models.ForeignKey(Enseignant, on_delete=models.CASCADE, blank=False,
-                                        null=True, db_column='id_enseignant_secondaire', to_field='id_enseignant', related_name='enseignant_secondaire')
-
-    class Meta:
-        db_table = 'emplacement'
 
 class Rapport_du_saisi(models.Model):
     id_rapport = models.AutoField(primary_key=True)
